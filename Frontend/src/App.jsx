73a4291c0227e81c6465c80015050a043cced188
saqibahmed13 <saqiba0484@gmail.com>
2024-05-30 22:02:38 +0530

    added

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+     <Navbar/>
+     <Banner/>
+     <Footer/>
+    </div>
+  )
+}
+
+export default App
