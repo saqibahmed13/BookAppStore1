bffebf0379f84965bdbd9be4298d9bfed5fd677e
saqibahmed13 <saqiba0484@gmail.com>
2024-06-03 10:11:13 +0530

    added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Banner from './components/Banner'
 import Footer from './components/Footer'
+import Freebooks from './components/Freebooks'
 
 function App() {
   return (
     <div>
      <Navbar/>
      <Banner/>
+     <Freebooks/>
      <Footer/>
     </div>
   )
--- Frontend/src/components/Freebooks.jsx
+++ Frontend/src/components/Freebooks.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React from 'react';
+import list from '../../public/list.json'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Card from './Card';
+
 
 function Freebooks() {
+const filterData = list.filter((data)=>data.category==="Free");
+var settings = {
+  dots: true,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  initialSlide: 0,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ] }
   return (
+    <>
+    <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
     <div>
-      
+    <h1 className='font-semibold text-xl pb-2 '>Free Offered Courses</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis harum quasi omnis ipsam unde maiores doloremque, odio, debitis eveniet doloribus nostrum ullam eius voluptatum quae velit dolorum beatae eaque fugiat.</p>
     </div>
+    <div>
+    <Slider {...settings}>
+        {filterData.map((item)=>(
+          <Card item={item} key={item.id}/>  // card k ander jo item hai item = {usme yeh value daldalo}
+        ))}
+      </Slider>
+        
+    </div>
+    </div>
+    </>
+
   )
-}
+}   
 
 export default Freebooks
