d46f25278978909237a6de4b146f95c247cd8370
saqibahmed13 <saqiba0484@gmail.com>
2024-06-05 11:50:38 +0530

    added

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -4,7 +4,7 @@ function Card({item}) {     // yahan wo map ka array aajata pura usme se hame jo
   return (
     <>
       <div className="mt-4 my-3 p-3">
-        <div className="card w-92 bg-base-100 shadow-xl hover:scale-105 duration-200">
+        <div className="card w-92 bg-base-100 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border ">
           <figure>
             <img
               src={item.image}    // item aaga parent(Freebooks) div se usme se humna image hona
--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Card from './Card'
 import list from '../../public/list.json'
+import {Link} from 'react-router-dom'
 function Course() {
   return (
     <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
@@ -14,7 +15,9 @@ function Course() {
           explicabo voluptatem est tenetur reprehenderit vel provident. Quas saepe qui vel modi delectus. Quaerat sit in vitae fugiat iusto, a autem explicabo! Culpa porro dolor ipsum iste veniam nulla itaque temporibus harum? Deleniti consequatur totam odit pariatur deserunt tempora. Blanditiis, minus eos quasi consequuntur nisi architecto aut molestias quisquam? Ullam sed tempore obcaecati et fugit! Aut libero voluptatem hic minima non odio ducimus expedita obcaecati.
           Ex possimus doloremque dicta, repellendus illum necessitatibus dolorum reiciendis voluptatem fugiat. Praesentium minima corporis quaerat explicabo, iure esse voluptatum fuga dolorem molestiae perferendis, vero deleniti eos animi labore nulla delectus ab quo! Repudiandae libero iusto iste provident perspiciatis dolorum aperiam iure accusamus tempore. Tempore, quaerat fuga. Ratione, voluptatum. Aspernatur cumque accusamus?
          </p>
+         <Link to='/'>
          <button className='mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300'>Back</button>
+         </Link>
       </div>
       <div className='mt-12 grid grid-cols-1 md:grid-cols-4'>
         {list.map((item)=>{
